34fed20c5924ccea24cb61544adfef6e35409483
Kemosabe2911 <stevinprince29@gmail.com>
2020-05-05 14:53:58 +0530

    Changed background

--- js/main.js
+++ js/main.js
@@ -1,11 +1,1 @@
-const spans =document.querySelector('span');
-spans.forEach(span => {span.addEventListener('mouseover',function(e){
-    span.classList.add('animated','rubberBand');
-})
-    
-});
-spans.forEach(span => {span.addEventListener('mouseout',function(e){
-    span.classList.remove('animated','rubberBand');
-})
-    
-});
+
