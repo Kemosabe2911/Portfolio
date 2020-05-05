ddaad96dd530bff18b0728135ef9cf29ec2faf7c
Kemosabe2911 <stevinprince29@gmail.com>
2020-05-05 15:01:55 +0530

    Update logo

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+var logo= document.getElementById('ttle');
+logo.addEventListener('mouseover',Kemosabe2911);
+logo.addEventListener('mouseleave',Stevin);
 
+function Stevin(){
+    logo.innerHTML=`<a href="#">Stevin Prince</a>`;
+}
+function Kemosabe2911(){
+    logo.innerHTML=`<a href="#">Kemosabe2911</a>`;
+}
