7f445c3180319323a6eb0394f279bd13540b91fe
Kemosabe2911 <stevinprince29@gmail.com>
2020-11-22 10:04:27 +0530

    Update home about

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ const themeMap = {
   document.getElementById('themeButton').onclick = toggleTheme;
 /*
 function pageTransition(){
-    var tl= gasp.timeline();
+    var tl= gsap.timeline();
 
     tl.to('ul.transition li', {duration: .5, scaleY: 1, transformOrigin: "bottom left", stagger: .2})
     tl.to('ul.transition li', {duration: .5, scaleY: 0, transformOrigin: "bottom left", stagger: .1, delay: .1})
