d7fd0e0e25a4006e34ae11bebae20422b1fdf70e
Kemosabe2911 <stevinprince29@gmail.com>
2020-11-22 10:53:58 +0530

    Update barba

--- js/main.js
+++ js/main.js
@@ -51,3 +51,35 @@ barba.init({
         }
     }]
 })*/
+
+function delay(n){
+    n = n || 2000;
+    return new Promise(done =>{
+        setTimeout(() =>{
+            done();
+        },n);
+    });
+}
+function pageTransition(){
+    var tl= gsap.timeline();
+
+    tl.to(".loading-screen",{
+        duration: 1, width: "100%" , left: "0%",ease: "Expo.easeInOut"
+    });
+    tl.to(".loading-screen",{
+        duration: 0.8, width: "100%" , left: "100%",ease: "Expo.easeInOut", delay: 0.3
+    });
+    tl.set(".loading-screen",{left: "-100%"});
+}
+barba.init({
+    sync: true,
+    transitions: [{
+        async leave(data){
+            const done = this.async();
+            
+            pageTransition();
+            await delay(1500);
+            done();
+        }
+    }]
+})
