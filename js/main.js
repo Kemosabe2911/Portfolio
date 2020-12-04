01171ce9eee20ad9f2fc10634907602317a9b27d
Kemosabe2911 <stevinprince29@gmail.com>
2020-12-04 16:39:22 +0530

    Add owl carousal

--- js/main.js
+++ js/main.js
@@ -96,3 +96,22 @@ barba.init({
         }
     }]
 })
+
+$(".info-content").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive:{
+        0:{
+            items: 1
+        },
+        600:{
+            items: 2
+        },
+        1000:{
+            items: 3
+        }
+    }
+})
